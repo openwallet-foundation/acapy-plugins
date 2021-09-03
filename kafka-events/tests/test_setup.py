d6aeeea5902a54df3c40589bff94b84fe1776eb6
Daniel Bluhm <dbluhm@pm.me>
2021-09-03 12:19:46 -0400

    test: setup
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ kafka-events/tests/test_setup.py
@@ -0,0 +1,30 @@
+"""Test setup."""
+from aiokafka import AIOKafkaProducer
+from aries_cloudagent.config.injection_context import InjectionContext
+from aries_cloudagent.core.event_bus import EventBus, MockEventBus
+from unittest import mock
+import pytest
+
+import kafka_queue as test_module
+
+
+@pytest.fixture
+def event_bus():
+    yield MockEventBus()
+
+
+@pytest.fixture
+def context(event_bus):
+    context = InjectionContext()
+    context.injector.bind_instance(EventBus, event_bus)
+    yield context
+
+
+@pytest.mark.asyncio
+async def test_setup(context, event_bus):
+    mock_producer = mock.MagicMock(spec=AIOKafkaProducer)
+    with mock.patch.object(
+        test_module, "AIOKafkaProducer", mock.MagicMock(return_value=mock_producer)
+    ):
+        await test_module.setup(context)
+        assert event_bus.topic_patterns_to_subscribers
