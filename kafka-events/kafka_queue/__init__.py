25e8ef5730f3c8120076cf83a1372c605c07d2d1
Daniel Bluhm <dbluhm@pm.me>
2021-09-29 14:47:44 -0400

    feat: use updated event with metadata to improve processing
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- kafka-events/kafka_queue/__init__.py
+++ kafka-events/kafka_queue/__init__.py
@@ -7,9 +7,9 @@ from string import Template
 from typing import Any, Mapping
 
 from aiokafka import AIOKafkaProducer
+from aries_cloudagent.config.base import BaseSettings
 from aries_cloudagent.config.injection_context import InjectionContext
-from aries_cloudagent.config.settings import Settings
-from aries_cloudagent.core.event_bus import Event, EventBus
+from aries_cloudagent.core.event_bus import EventBus, EventWithMetadata
 from aries_cloudagent.core.profile import Profile
 
 DEFAULT_CONFIG = {
@@ -27,11 +27,11 @@ DEFAULT_CONFIG = {
 LOGGER = logging.getLogger(__name__)
 
 
-def get_config(settings: Settings) -> Mapping[str, Any]:
+def get_config(settings: BaseSettings) -> Mapping:
     """Retrieve producer configuration from settings."""
     try:
         producer_conf = (
-            settings["plugin_config"]["kafka_queue"]["producer-config"]
+            settings["plugin_config"]["kafka_queue"]
             or DEFAULT_CONFIG
         )
     except KeyError:
@@ -44,24 +44,25 @@ async def setup(context: InjectionContext):
     """Setup the plugin."""
     config = get_config(context.settings)
     LOGGER.info(f"Setting up kafka plugin with configuration: {config}")
-    producer = AIOKafkaProducer(**config.get("producer"))
+    producer = AIOKafkaProducer(**config.get("producer", {}))
     LOGGER.info("  - Starting kafka producer")
     await producer.start()
 
     # Add the Kafka producer in the context
     context.injector.bind_instance(AIOKafkaProducer, producer)
     LOGGER.info("   - Subscribing Kafka producer to eventbus events")
+
     # Handle event for Kafka
     bus = context.inject(EventBus)
     if not bus:
         raise ValueError("EventBus missing in context")
 
-    for event in config.get("outbound_topic_templates"):
+    for event in config.get("outbound_topic_templates", {}):
         LOGGER.info(f"      - subscribing to event: {event}")
         bus.subscribe(re.compile(event), handle_event)
 
 
-async def handle_event(profile: Profile, event: Event):
+async def handle_event(profile: Profile, event: EventWithMetadata):
     """
     produce kafka events from eventbus events
     """
@@ -74,14 +75,12 @@ async def handle_event(profile: Profile, event: Event):
     event.payload["wallet_id"] = profile.settings.get("wallet.id")
     config = get_config(profile.settings)
     try:
-        for pattern, template in config.get("outbound_topic_templates").items():
-            if re.match(pattern, event.topic):
-                topic = Template(template).substitute(**event.payload)
-                break
-        LOGGER.info(f"Sending message {event.payload} with Kafka topic {topic}")
+        template = config.get("outbound_topic_templates", {})[event.metadata.pattern.pattern]
+        kafka_topic = Template(template).substitute(**event.payload)
+        LOGGER.info(f"Sending message {event.payload} with Kafka topic {kafka_topic}")
         # Produce message
         await producer.send_and_wait(
-            topic,
+            kafka_topic,
             str.encode(json.dumps(event.payload)),
             key=profile.settings.get("wallet.id"),
         )
