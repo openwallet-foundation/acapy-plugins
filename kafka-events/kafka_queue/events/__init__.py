c111c50351bcd3a2c4c70ee845c51ad0c0f79b52
Daniel Bluhm <dbluhm@pm.me>
2021-11-03 01:47:02 -0400

    feat: events use single producer
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- kafka-events/kafka_queue/events/__init__.py
+++ kafka-events/kafka_queue/events/__init__.py
@@ -7,8 +7,9 @@ from string import Template
 from typing import Optional, cast
 
 from aiokafka import AIOKafkaProducer
-from aries_cloudagent.core.event_bus import EventBus, EventWithMetadata
+from aries_cloudagent.core.event_bus import Event, EventBus, EventWithMetadata
 from aries_cloudagent.core.profile import Profile
+from aries_cloudagent.core.util import SHUTDOWN_EVENT_PATTERN, STARTUP_EVENT_PATTERN
 from aries_cloudagent.config.injection_context import InjectionContext
 
 from ..config import get_config, EventsConfig
@@ -31,9 +32,24 @@ async def setup(context: InjectionContext):
         LOGGER.info(f"subscribing to event: {event}")
         bus.subscribe(re.compile(event), handle_event)
 
+    bus.subscribe(STARTUP_EVENT_PATTERN, on_startup)
+    bus.subscribe(SHUTDOWN_EVENT_PATTERN, on_shutdown)
+
 
 RECORD_RE = re.compile(r"acapy::record::([^:]*)(?:::(.*))?")
 WEBHOOK_RE = re.compile(r"acapy::webhook::{.*}")
+
+
+async def on_startup(profile: Profile, event: Event):
+    config = get_config(profile.settings).events or EventsConfig.default()
+    producer = AIOKafkaProducer(**config.producer.dict())
+    profile.context.injector.bind_instance(AIOKafkaProducer, producer)
+    await producer.start()
+
+
+async def on_shutdown(profile: Profile, event: Event):
+    producer = profile.inject(AIOKafkaProducer)
+    await producer.stop()
 
 
 def _derive_category(topic: str):
@@ -46,6 +62,7 @@ def _derive_category(topic: str):
 
 async def handle_event(profile: Profile, event: EventWithMetadata):
     """Produce kafka events from aca-py events."""
+    producer = profile.inject(AIOKafkaProducer)
 
     LOGGER.info("Handling Kafka producer event: %s", event)
     wallet_id = cast(Optional[str], profile.settings.get("wallet.id"))
@@ -62,11 +79,10 @@ async def handle_event(profile: Profile, event: EventWithMetadata):
         kafka_topic = Template(template).substitute(**payload)
         LOGGER.info(f"Sending message {payload} with Kafka topic {kafka_topic}")
         # Produce message
-        async with AIOKafkaProducer(**config.producer.dict()) as producer:
-            await producer.send_and_wait(
-                kafka_topic,
-                str.encode(json.dumps(payload)),
-                key=wallet_id.encode() if wallet_id else None,
-            )
+        await producer.send_and_wait(
+            kafka_topic,
+            str.encode(json.dumps(payload)),
+            key=wallet_id.encode() if wallet_id else None,
+        )
     except Exception:
         LOGGER.exception("Kafka producer failed to send message")
