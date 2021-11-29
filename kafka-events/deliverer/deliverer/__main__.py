31eee4b0f0d32149b66744dbcd8255aae127804d
Adam Burdett <burdettadam@gmail.com>
2021-11-29 16:54:43 -0700

    some suggestions for retry
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- kafka-events/deliverer/deliverer/__main__.py
+++ kafka-events/deliverer/deliverer/__main__.py
@@ -2,14 +2,14 @@
 import asyncio
 import base64
 import json
-from os import getenv
 import sys
-from urllib.parse import urlparse
+from os import getenv
 from typing import Dict
-from pydantic import BaseModel, PrivateAttr, validator
+from urllib.parse import urlparse
 
 import aiohttp
-from aiokafka import AIOKafkaConsumer, ConsumerRecord
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer, ConsumerRecord
+from pydantic import BaseModel, PrivateAttr, validator
 
 
 DEFAULT_BOOTSTRAP_SERVER = "kafka"
@@ -59,6 +59,7 @@ class OutboundPayload(BaseModel):
 
 
 class DelayPayload(BaseModel):
+    topic: str
     retries: int
     outbound: OutboundPayload
 
@@ -71,8 +72,7 @@ class DelayPayload(BaseModel):
     def to_queue(self) -> bytes:
         return str.encode(self.json(), encoding="utf8")
 
-
-async def main():
+async def consume_http_message():
     consumer = AIOKafkaConsumer(
         OUTBOUND_TOPIC, bootstrap_servers=BOOTSTRAP_SERVER, group_id=GROUP
     )
@@ -80,11 +80,8 @@ async def main():
     async with consumer:
         async for msg in consumer:
             outbound = OutboundPayload.from_queue(msg)
-            if (
-                outbound.endpoint_scheme == "http"
-                or outbound.endpoint_scheme == "https"
-            ):
-                print(f"Dispatch message to {outbound.endpoint}", flush=True)
+            if outbound.endpoint_scheme in ["http", "https"]:
+                print(f'Dispatch message to {outbound.endpoint}', flush=True)
                 try:
                     response = await http_client.post(
                         outbound.endpoint,
@@ -93,10 +90,50 @@ async def main():
                         timeout=10,
                     )
                 except aiohttp.ClientError as err:
-                    log_error("Delivery error:", err)
+                    log_error('Delivery error:', err)
                 else:
                     if response.status < 200 or response.status >= 300:
+                        # produce delay_payload kafka event
+                        async with AIOKafkaProducer({}) as producer:
+                            payload = DelayPayload.to_queue(
+                                { **outbound,
+                                 "topic": msg.topic,
+                                 "retries": outbound.retries + 1
+                                } 
+                            )
+                            await producer.send_and_wait(
+                                'delay_payload',
+                                payload
+                            )
                         log_error("Invalid response code:", response.status)
+
+async def retry_kafka_to_http_msg():
+    consumer = AIOKafkaConsumer(
+        "delay_payload", bootstrap_servers=BOOTSTRAP_SERVER, group_id=GROUP
+    )
+    async with consumer:
+        async for msg in consumer:
+            print(f"Processing delay_payload msg: {msg}")
+            payload = DelayPayload.from_queue(msg)
+            # todo: add configuration for number of retry attempts
+            if (payload.retries < 4):
+                await asyncio.sleep(2 ** payload.retries)
+                async with AIOKafkaProducer({}) as producer:
+                    payload = OutboundPayload.to_queue(
+                        { **payload,
+                            "retries": payload.retries
+                        } 
+                    )
+                    del payload['topic']
+                    await producer.send_and_wait(
+                        payload.topic,
+                        payload,
+                    )
+            # else, we do not handle the event again.
+
+async def main():
+    kafka_to_http_task = asyncio.create_task(consume_http_message())
+    retry_kafka_to_http_msg_task = asyncio.create_task(retry_kafka_to_http_msg())
 
 
 if __name__ == "__main__":
