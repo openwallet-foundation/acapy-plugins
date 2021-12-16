6059b857992fe93a718a4b2a3af918213a417254
Adam Burdett <burdettadam@gmail.com>
2021-12-16 11:46:49 -0700

    requested changes for pr
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- kafka-events/deliverer/deliverer/__main__.py
+++ kafka-events/deliverer/deliverer/__main__.py
@@ -6,7 +6,7 @@ from contextlib import suppress
 from os import getenv
 from typing import List
 
-import aiohttp
+from aiohttp import ClientError, ClientSession, DummyCookieJar
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 
 from . import OutboundPayload
@@ -24,61 +24,57 @@ def log_error(*args):
     print(*args, file=sys.stderr)
 
 
-async def consume_http_message():
+async def _produce_failure_and_commit(producer, consumer, msg):
+    await producer.send_and_wait("failed_outbound_message", msg.value)
+    await consumer.commit()
+
+
+async def _deliver_http_message(msg, outbound, producer, consumer, client):
+    for retries in range(MAX_RETRIES):
+        try:
+            if retries >= (MAX_RETRIES - 1):
+                log_error("Failed outbound message, to many attempts.")
+                await _produce_failure_and_commit(producer, consumer, msg)
+                break
+            async with client.post(
+                outbound.endpoint,
+                data=outbound.payload,
+                headers=outbound.headers,
+                timeout=10,
+            ) as response:
+                if response.status < 200 or response.status >= 300:
+                    log_error("Invalid response code:", response.status)
+                    await asyncio.sleep(2 ** retries)
+                else:
+                    await consumer.commit()
+                    break
+        except ClientError as err:
+            log_error("Delivery error:", err)
+            await _produce_failure_and_commit(producer, consumer, msg)
+
+
+async def main():
     consumer = AIOKafkaConsumer(
         OUTBOUND_TOPIC,
         bootstrap_servers=BOOTSTRAP_SERVER,
         group_id=GROUP,
         enable_auto_commit=False,
     )
+    producer = AIOKafkaProducer(
+        bootstrap_servers=BOOTSTRAP_SERVER,
+        enable_idempotence=True,
+    )
 
-    async with aiohttp.ClientSession(
-        cookie_jar=aiohttp.DummyCookieJar()
-    ) as http_client:
-        async with consumer:
-            async for msg in consumer:
-                outbound = OutboundPayload.from_bytes(msg.value)
-                if outbound.endpoint_scheme in ["http", "https"]:
-                    print(f"Dispatch message to {outbound.endpoint}", flush=True)
-                    for retries in range(MAX_RETRIES):
-                        try:
-                            if retries > MAX_RETRIES:
-                                log_error("Failed outbound message, to many attempts.")
-                                async with AIOKafkaProducer(
-                                    bootstrap_servers=BOOTSTRAP_SERVER,
-                                    enable_idempotence=True,
-                                ) as producer:
-                                    await producer.send_and_wait(
-                                        "failed_outbound_message", msg.value
-                                    )
-                                await consumer.commit()
-                                break
-                            async with http_client.post(
-                                outbound.endpoint,
-                                data=outbound.payload,
-                                headers=outbound.headers,
-                                timeout=10,
-                            ) as response:
-                                if response.status < 200 or response.status >= 300:
-                                    await asyncio.sleep(2 ** retries)
-                                    log_error("Invalid response code:", response.status)
-                                else:
-                                    await consumer.commit()
-                                    break
-                        except aiohttp.ClientError as err:
-                            log_error("Delivery error:", err)
-                            async with AIOKafkaProducer(
-                                bootstrap_servers=BOOTSTRAP_SERVER,
-                                enable_idempotence=True,
-                            ) as producer:
-                                await producer.send_and_wait(
-                                    "failed_outbound_message", msg.value
-                                )
-                                await consumer.commit()
-
-
-async def main():
-    await consume_http_message()
+    async with ClientSession(
+        cookie_jar=DummyCookieJar()
+    ) as http_client, consumer, producer:
+        async for msg in consumer:
+            outbound = OutboundPayload.from_bytes(msg.value)
+            if outbound.endpoint_scheme in ["http", "https"]:
+                print(f"Dispatch message to {outbound.endpoint}", flush=True)
+                await _deliver_http_message(
+                    msg, outbound, producer, consumer, http_client
+                )
 
 
 if __name__ == "__main__":
