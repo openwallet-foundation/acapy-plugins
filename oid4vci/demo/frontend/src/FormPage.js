d37f70aba1fcb7822191b9709bf904bb499183c8
Adam Burdett <burdettadam@gmail.com>
2023-10-23 14:41:58 -0600

    feat: demo frontend with docker compose
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

---
+++ oid4vci/demo/frontend/src/FormPage.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+import img1 from "./img/img1.png";
+import img2 from "./img/img2.png";
+import img3 from "./img/img3.png";
+
+const FormPage = () => {
+  const navigate = useNavigate();
+  const [selectedCredential, setSelectedCredential] = useState("");
+  const { state } = useLocation();
+  const { firstName, lastName, email } = state;
+
+  const handleCredentialSelect = (credential) => {
+    setSelectedCredential(credential);
+  };
+
+  const handleSubmit = () => {
+    console.log(
+      `/qr-code/${firstName}/${lastName}/${email}/${selectedCredential}`
+    );
+    // Set the Axios configuration for CORS and credentials
+    axios.defaults.withCredentials = true; // Enable credentials (cookies, etc.)
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:3001'; // Adjust the origin as needed
+
+    // api call to controller, `POST /exchange/submit`
+    axios
+      .post("http://localhost:3001/oid4vci/create", {
+        name: firstName,
+        lastname: lastName,
+        email,
+        credential: selectedCredential,
+      })
+      .then((response) => {
+        // TODO: call offer endpoint
+        console.log(response.data);
+        const credentialOffer = response.data.offer;
+        const {exchange_id} = response.data;
+        navigate(`/qr-code`, { state: { credentialOffer, exchange_id: exchange_id } });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Select the Credential You Would Like to Collect</h2>
+      <button onClick={() => navigate("/")}>Back</button>
+      <div className="credential-options">
+        <img
+          src={img1}
+          width={250}
+          height={150}
+          alt="UniversityDegreeCredential"
+          onClick={() => handleCredentialSelect("UniversityDegreeCredential")}
+        />
+        <img
+          src={img2}
+          width={250}
+          height={150}
+          alt="Credential 2"
+          onClick={() => handleCredentialSelect("Credential 2")}
+        />
+        <img
+          src={img3}
+          width={250}
+          height={150}
+          alt="Credential 3"
+          onClick={() => handleCredentialSelect("Credential 3")}
+        />
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default FormPage;
---
+++ oid4vci/demo/frontend/src/InputForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+const InputForm = () => {
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleShareClick = () => {
+    navigate(`/credentials`,{ state: {firstName:firstName, lastName:lastName, email:email}});
+    
+  };
+
+  return (
+    <div>
+      <h2>Input Form</h2>
+      
+      <form>
+      <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={handleLastNameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+        <button type="button" onClick={handleShareClick}>
+          Share
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputForm;
