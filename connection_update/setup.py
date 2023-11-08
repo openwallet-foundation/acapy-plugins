4e232e17004cde222604da84ff28ae9ee66f5e30
jamshale <jamiehalebc@gmail.com>
2023-11-08 10:26:06 -0800

    Move pytest ignore warnings and remove setup files
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- connection_update/setup.py
+++
@@ -1,6 +0,0 @@
-"""Setup file for connection_update."""
-#!/usr/bin/env python
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
--- redis_events/setup.py
+++
@@ -1,6 +0,0 @@
-"""Setup file for redis_events."""
-#!/usr/bin/env python
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
