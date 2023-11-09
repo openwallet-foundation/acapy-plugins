259b9deb32b093318579855937763c0c413d8464
Daniel Bluhm <dbluhm@pm.me>
2023-11-09 10:30:31 -0500

    feat: message buffering and TCP
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- oid4vci/int/afj-wrapper/afj_wrapper/jsonrpc.py
+++ oid4vci/int/afj-wrapper/afj_wrapper/jsonrpc.py
@@ -1,3 +1,5 @@
+"""JSON-RPC client implementation."""
+from abc import ABC, abstractmethod
 import asyncio
 import json
 from typing import Any, Dict, Optional, Protocol
@@ -8,20 +10,26 @@ class TransportProtocol(Protocol):
     """Transport protocol that transport class should implement."""
 
     async def send(self, message: str) -> None:
+        """Send a message to connected clients."""
         ...
 
     async def receive(self) -> str:
+        """Receive a message from connected clients."""
         ...
 
 
 class JsonRpcError(Exception):
+    """Exception raised when an error is returned by the server."""
+
     def __init__(self, error: dict):
+        """Initialize the exception."""
         self.code = error.get("code")
         self.message = error.get("message")
         self.data = error.get("data")
         super().__init__(self.__str__())
 
     def __str__(self) -> str:
+        """Convert the exception to a string."""
         error_str = f"JSON-RPC Error {self.code}: {self.message}"
         if isinstance(self.data, str):
             # Represent newlines in data correctly when converting to string
@@ -34,6 +42,7 @@ class JsonRpcClient:
     """JSON-RPC client implementation."""
 
     def __init__(self, transport: TransportProtocol) -> None:
+        """Initialize the client."""
         self.transport = transport
         self.id_counter = 0
         self.pending_calls: Dict[int, asyncio.Future] = {}
@@ -77,9 +86,14 @@ class JsonRpcClient:
         """Async context manager: close the client."""
         await self.stop()
 
+    async def request(self, method: str, **params: Any) -> Any:
+        """Send a request to the server."""
+        return await self.send_request(method, params)
+
     async def send_request(
         self, method: str, params: Optional[Dict[str, Any]] = None
     ) -> Any:
+        """Send a request to the server."""
         self.id_counter += 1
         message_id = self.id_counter
         request = {
@@ -96,6 +110,7 @@ class JsonRpcClient:
         return await future
 
     async def receive_response(self) -> None:
+        """Receive responses from the server."""
         while True:
             response_str = await self.transport.receive()
             response = json.loads(response_str)
@@ -113,42 +128,92 @@ class JsonRpcClient:
                     future.set_exception(Exception("Invalid JSON-RPC response"))
 
 
-class UnixSocketTransport:
-    """Transport implementation that uses a Unix socket."""
+class BaseSocketTransport(ABC):
+    """Base transport implementation using asyncio sockets."""
 
-    def __init__(self, path: str) -> None:
-        self.path = path
+    def __init__(self) -> None:
+        """Initialize the transport."""
         self.reader: Optional[asyncio.StreamReader] = None
         self.writer: Optional[asyncio.StreamWriter] = None
 
-    async def connect(self) -> None:
-        self.reader, self.writer = await asyncio.open_unix_connection(self.path)
-
     async def send(self, message: str) -> None:
+        """Send a message to connected clients."""
         if self.writer is None:
             raise Exception("Transport is not connected")
-        self.writer.write(message.encode())
+
+        message_bytes = message.encode()
+        header = f"length: {len(message_bytes)}\n".encode()
+        full_message = header + message_bytes
+
+        self.writer.write(full_message)
         await self.writer.drain()
 
     async def receive(self) -> str:
+        """Receive a message from connected clients."""
         if self.reader is None:
             raise Exception("Transport is not connected")
-        data = await self.reader.read(4096)  # Adjust buffer size as needed
-        return data.decode()
+
+        # Read the header first
+        header = await self.reader.readuntil(b"\n")
+        # Decode the header and extract the length
+        length_str = header.decode().strip()
+        if not length_str.startswith("length: "):
+            raise ValueError("Invalid message header received")
+
+        # Extract the length and convert to an integer
+        length = int(length_str.split("length: ")[1].strip())
+
+        # Now read the body of the message
+        body_bytes = await self.reader.readexactly(length)
+
+        # Decode and return the message body
+        return body_bytes.decode()
 
     async def close(self) -> None:
+        """Close the transport."""
         if self.writer is not None:
             self.writer.close()
             await self.writer.wait_closed()
 
-    async def __aenter__(self) -> "UnixSocketTransport":
-        """Async context manager: connect to the socket."""
+    async def __aenter__(self) -> "BaseSocketTransport":
+        """Async context manager: connect to the server."""
         await self.connect()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
-        """Async context manager: close the socket."""
+        """Async context manager: close the transport."""
         await self.close()
+
+    @abstractmethod
+    async def connect(self):
+        """Connect to the server."""
+
+
+class UnixSocketTransport(BaseSocketTransport):
+    """Transport implementation that uses a Unix socket."""
+
+    def __init__(self, path: str) -> None:
+        """Initialize the transport."""
+        super().__init__()
+        self.path = path
+
+    async def connect(self) -> None:
+        """Connect to the server."""
+        self.reader, self.writer = await asyncio.open_unix_connection(self.path)
+
+
+class TCPSocketTransport(BaseSocketTransport):
+    """Transport implementation that uses a TCP socket."""
+
+    def __init__(self, host: str, port: int) -> None:
+        """Initialize the transport."""
+        super().__init__()
+        self.host = host
+        self.port = port
+
+    async def connect(self) -> None:
+        """Connect to the server."""
+        self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
 
 
 async def main():
@@ -158,10 +223,13 @@ async def main():
         """Dummy transport implementation that prints messages to the console."""
 
         async def send(self, message: str) -> None:
+            """Send a message to connected clients."""
             print(f"Sending message: {message}")
 
         async def receive(self) -> str:
-            # Simulate a response (In a real implementation, you would receive messages from a server)
+            """Receive a message from connected clients."""
+            # Simulate a response (In a real implementation, you would receive
+            # messages from a server)
             await asyncio.sleep(1)
             return json.dumps({"jsonrpc": "2.0", "result": "pong", "id": 1})
 
--- oid4vci/int/afj/src/server.ts
+++ oid4vci/int/afj/src/server.ts
@@ -1,39 +1,104 @@
 import { createServer, Server, Socket } from 'net';
 
+class MessageBuffer {
+  private buffer: Buffer = Buffer.alloc(0);
+  private headerSize: number | null = null;
+  public onMessage?: (message: string) => void = undefined;
+
+  public append(data: Buffer): void {
+    this.buffer = Buffer.concat([this.buffer, data]);
+    this.processBuffer();
+  }
+
+  private processBuffer(): void {
+    while (true) {
+      if (this.headerSize === null) {
+        const headerEnd = this.buffer.indexOf('\n');
+        if (headerEnd !== -1) {
+          const header = this.buffer.subarray(0, headerEnd).toString();
+          const match = header.match(/^length: (\d+)/);
+          if (match) {
+            this.headerSize = Number(match[1]);
+            this.buffer = this.buffer.subarray(headerEnd + 1); // +1 for the new line
+          } else {
+            throw new Error('Invalid message header');
+          }
+        } else {
+          // Not enough data to read the header yet
+          break;
+        }
+      }
+
+      // If we have determined the headerSize, see if we have enough buffer to read it
+      if (this.headerSize !== null && this.buffer.length >= this.headerSize) {
+        const message = this.buffer.subarray(0, this.headerSize).toString();
+        this.buffer = this.buffer.subarray(this.headerSize);
+        this.headerSize = null; // Reset for the next message
+
+        // Emit the 'message' event with the complete message
+        if (!this.onMessage) {
+          throw new Error('No message handler set');
+        }
+        this.onMessage(message);
+      } else {
+        // Not enough data to read the full message yet
+        break;
+      }
+    }
+  }
+}
+
 interface ServerConfig {
   socketPath: string;
 }
 
 type Handler = (data: string) => Promise<void>;
 
-export class SocketServer {
-  private config: ServerConfig;
-  private server: Server | undefined;
-  private handlers: Handler[] = [];
-  private closeHandlers: (() => void)[] = [];
-  private sockets: Set<Socket> = new Set();
+export abstract class BaseSocketServer {
+  protected server: Server;
+  protected handlers: Handler[] = [];
+  protected closeHandlers: (() => void)[] = [];
+  protected sockets: Set<Socket> = new Set();
 
-  constructor(config: ServerConfig) {
-    this.config = config;
+  constructor() {
+    this.server = createServer(socket => {
+      const messageBuffer = new MessageBuffer();
+
+      messageBuffer.onMessage = (message) => {
+        for (const handler of this.handlers) {
+          handler(message).then();
+        }
+      }
+
+      socket.on('data', data => {
+        messageBuffer.append(data);
+      });
+
+      socket.on('close', () => {
+        this.sockets.delete(socket);
+        for (const handler of this.closeHandlers) {
+          handler();
+        }
+      });
+
+      socket.on('error', (err) => {
+        console.error('Socket error:', err);
+      });
+
+      this.sockets.add(socket);
+    });
   }
 
-  public start(): void {
-    this.startSocketServer();
-  }
+  public abstract start(): void;
 
   public stop(): void {
-    this.server?.close(function() {
+    this.server.close(() => {
       console.log('Closed out remaining connections.');
-      // End the process with success code (0)
-      process.exit(0);
     });
 
-    // If server is not finished within a certain time limit, 
-    // force close the server and exit
-    setTimeout(function() {
+    setTimeout(() => {
       console.error('Could not close connections in time, forcefully shutting down');
-      process.exit(1);
-    }, 10000); // 10 seconds
+    }, 10000);
   }
 
   public ondata(handler: Handler): void {
@@ -45,31 +110,53 @@ export class SocketServer {
   }
 
   public send(data: string): void {
+    const buffer = Buffer.from(data);
+    const header = `length: ${buffer.length}\n`;
+    const headerBuffer = Buffer.from(header);
+    const message = Buffer.concat([headerBuffer, buffer]);
+
     for (const socket of this.sockets) {
-      socket.write(data);
+      socket.write(message);
     }
   }
+}
 
-  private startSocketServer(): void {
-    this.server = createServer(socket => {
-      this.sockets.add(socket);
-      
-      socket.on('data', data => {
-        for (const handler of this.handlers) {
-          handler(data.toString()).then();
-        }
-      });
+interface UnixServerConfig {
+  socketPath: string;
+}
 
-      socket.on('close', () => {
-        this.sockets.delete(socket);
-        for (const handler of this.closeHandlers) {
-          handler();
-        }
-      });
-    });
+export class UnixSocketServer extends BaseSocketServer {
+  private config: UnixServerConfig;
 
+  constructor(config: UnixServerConfig) {
+    super();
+    this.config = config;
+  }
+
+  public start(): void {
     this.server.listen(this.config.socketPath, () => {
-      console.log(`Server listening on socket ${this.config.socketPath}`);
+      console.log(`Server listening on Unix socket ${this.config.socketPath}`);
+    });
+  }
+}
+
+
+interface TCPServerConfig {
+  host: string;
+  port: number;
+}
+
+export class TCPSocketServer extends BaseSocketServer {
+  private config: TCPServerConfig;
+
+  constructor(config: TCPServerConfig) {
+    super();
+    this.config = config;
+  }
+
+  public start(): void {
+    this.server.listen(this.config.port, this.config.host, () => {
+      console.log(`Server listening on TCP ${this.config.host}:${this.config.port}`);
     });
   }
 }
