4f38e504498f36e3b5278871e6a8219265202e30
Daniel Bluhm <dbluhm@pm.me>
2023-11-26 18:22:41 -0500

    refactor: minimize oid4vci server responsibilities
    I think some of the previous endpoints are undesirable for public
    endpoints.
    
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- oid4vci/oid4vci/oid4vci_server.py
+++ oid4vci/oid4vci/oid4vci_server.py
@@ -2,51 +2,33 @@
 
 import logging
 
-import aiohttp_cors
 from aiohttp import web
-from aiohttp_apispec import (
-    docs,
-    response_schema,
-    setup_aiohttp_apispec,
-    validation_middleware,
-)
+from aiohttp_apispec import setup_aiohttp_apispec, validation_middleware
+import aiohttp_cors
 from aries_cloudagent.admin.base_server import BaseAdminServer
 from aries_cloudagent.admin.error import AdminSetupError
 from aries_cloudagent.admin.request_context import AdminRequestContext
 from aries_cloudagent.admin.server import debug_middleware, ready_middleware
 from aries_cloudagent.config.injection_context import InjectionContext
 from aries_cloudagent.core.profile import Profile
-from aries_cloudagent.messaging.models.openapi import OpenAPISchema
-from aries_cloudagent.utils.stats import Collector
-from marshmallow import fields
 
 from .public_routes import register as public_routes_register
 
 LOGGER = logging.getLogger(__name__)
 
 
-class AdminResetSchema(OpenAPISchema):
-    """Schema for the reset endpoint."""
-
-
-class AdminStatusLivelinessSchema(OpenAPISchema):
-    """Schema for the liveliness endpoint."""
-
-    alive = fields.Boolean(
-        metadata={"description": "Liveliness status", "example": True}
-    )
-
-
-class AdminStatusReadinessSchema(OpenAPISchema):
-    """Schema for the readiness endpoint."""
-
-    ready = fields.Boolean(
-        metadata={"description": "Readiness status", "example": True}
-    )
-
-
 class Oid4vciServer(BaseAdminServer):
-    """Oid4vci HTTP server class."""
+    """Server for OpenID4VCI routes.
+
+    This server must be separate from the Admin API of ACA-Py because the
+    audience is different. ACA-Py's Admin API is meant for internal use only.
+    It's endpoints are secured by either API Key or by firewall rules. In contrast,
+    the OpenID4VCI routes must be publicly accessible. The authorization for these
+    routes are controlled by knowledge of codes and tokens as passed during the
+    protocol implemented by the endpoints.
+
+    In addition to the public routes, this server also provides a health check
+    """
 
     def __init__(
         self,
@@ -76,12 +58,18 @@ class Oid4vciServer(BaseAdminServer):
 
         @web.middleware
         async def setup_context(request: web.Request, handler):
-            profile = self.profile
+            """Set up request context.
 
+            TODO: support Multitenancy context setup
+            Right now, this will only work for a standard agent instance. To
+            support multitenancy, we will need to include wallet identifiers in
+            the path and report that path in credential offers and issuer
+            metadata from a tenant.
+            """
             admin_context = AdminRequestContext(
-                profile=profile,
+                profile=self.profile,
                 # root_profile=self.profile, # TODO: support Multitenancy context setup
-                # metadata={},# TODO: support Multitenancy context setup
+                # metadata={}, # TODO: support Multitenancy context setup
             )
             request["context"] = admin_context
             return await handler(request)
@@ -90,7 +78,8 @@ class Oid4vciServer(BaseAdminServer):
 
         app = web.Application(
             middlewares=middlewares,
-            client_max_size=(  # TODO: update settings for oid4vci
+            # TODO: Do these values need to be tweaked for OpenID4VCI?
+            client_max_size=(
                 self.context.settings.get("admin.admin_client_max_request_size", 1)
                 * 1024
                 * 1024
@@ -99,10 +88,8 @@ class Oid4vciServer(BaseAdminServer):
 
         app.add_routes(
             [
+                # TODO: No swagger in production?
                 web.get("/", self.redirect_handler, allow_head=True),
-                web.post("/status/reset", self.status_reset_handler),
-                web.get("/status/live", self.liveliness_handler, allow_head=False),
-                web.get("/status/ready", self.readiness_handler, allow_head=False),
             ]
         )
 
@@ -123,12 +110,11 @@ class Oid4vciServer(BaseAdminServer):
             cors.add(route)
 
         # get agent label
-        agent_label = self.context.settings.get("default_label")
-        __version__ = 11  # TODO: get dynamically from config
+        __version__ = 0  # TODO: get dynamically from config
         version_string = f"v{__version__}"
 
         setup_aiohttp_apispec(
-            app=app, title=agent_label, version=version_string, swagger_path="/api/doc"
+            app=app, title="OpenID4VCI", version=version_string, swagger_path="/api/doc"
         )
 
         # ensure we always have status values
@@ -168,65 +154,6 @@ class Oid4vciServer(BaseAdminServer):
             await self.site.stop()
             self.site = None
 
-    @docs(tags=["server"], summary="Reset statistics")
-    @response_schema(AdminResetSchema(), 200, description="")
-    async def status_reset_handler(self, request: web.BaseRequest):
-        """Request handler for resetting the timing statistics.
-
-        Args:
-            request: aiohttp request object
-
-        Returns:
-            The web response
-
-        """
-        collector = self.context.inject_or(Collector)
-        if collector:
-            collector.reset()
-        return web.json_response({})
-
     async def redirect_handler(self, request: web.BaseRequest):
         """Perform redirect to documentation."""
         raise web.HTTPFound("/api/doc")
-
-    @docs(tags=["server"], summary="Liveliness check")
-    @response_schema(AdminStatusLivelinessSchema(), 200, description="")
-    async def liveliness_handler(self, request: web.BaseRequest):
-        """Request handler for liveliness check.
-
-        Args:
-            request: aiohttp request object
-
-        Returns:
-            The web response, always indicating True
-
-        """
-        app_live = self.app._state["alive"]
-        if app_live:
-            return web.json_response({"alive": app_live})
-        else:
-            raise web.HTTPServiceUnavailable(reason="Service not available")
-
-    @docs(tags=["server"], summary="Readiness check")
-    @response_schema(AdminStatusReadinessSchema(), 200, description="")
-    async def readiness_handler(self, request: web.BaseRequest):
-        """Request handler for liveliness check.
-
-        Args:
-            request: aiohttp request object
-
-        Returns:
-            The web response, indicating readiness for further calls
-
-        """
-        app_ready = self.app._state["ready"] and self.app._state["alive"]
-        if app_ready:
-            return web.json_response({"ready": app_ready})
-        else:
-            raise web.HTTPServiceUnavailable(reason="Service not ready")
-
-    def notify_fatal_error(self):
-        """Set our readiness flags to force a restart (openshift)."""
-        LOGGER.error("Received shutdown request notify_fatal_error()")
-        self.app._state["ready"] = False
-        self.app._state["alive"] = False
