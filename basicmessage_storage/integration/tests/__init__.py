4a2d493289328e028605e2be69323faf3f57964d
Jason Syrotuck <jasyrotuck@gmail.com>
2023-12-13 15:55:12 -0800

    clean up unused for now
    Signed-off-by: Jason Syrotuck <jasyrotuck@gmail.com>

--- basicmessage_storage/integration/tests/__init__.py
+++ basicmessage_storage/integration/tests/__init__.py
@@ -13,16 +13,14 @@ def get(agent: str, path: str, **kwargs):
     """Get."""
     return requests.get(f"{agent}{path}", **kwargs)
 
-def put(agent: str, path: str, **kwargs):
-    """Post."""
-    return requests.put(f"{agent}{path}", **kwargs)
 
 def post(agent: str, path: str, **kwargs):
     """Post."""
     return requests.post(f"{agent}{path}", **kwargs)
 
+
 def delete(agent: str, path: str, **kwargs):
-    """Post."""
+    """Delete."""
     return requests.delete(f"{agent}{path}", **kwargs)
 
 
@@ -87,7 +85,6 @@ class Agent:
     def retrieve_basicmessages(self, **kwargs):
         """Retrieve connections."""
         return get(self.url, "/basicmessages", params=kwargs)
-    
 
     @unwrap_json_response
     @fail_if_not_ok("Failed to delete basic messages")
@@ -102,35 +99,6 @@ class Agent:
         return post(
             self.url,
             f"/connections/{connection_id}/send-message",
-            json={"content": content},
-        )
-    
-    @unwrap_json_response
-    @fail_if_not_ok("Failed to get wallet settings")
-    def get_subwallets(self):
-        """Set connection metadata."""
-        return get(
-            self.url,
-            f"/multitenancy/wallets"
-        )
-
-    @unwrap_json_response
-    @fail_if_not_ok("Failed to get wallet settings")
-    def get_subwallet_settings(self, wallet_id, content):
-        """Set connection metadata."""
-        return get(
-            self.url,
-            f"/multitenancy/wallet/{wallet_id}/",
-            json={"content": content},
-        )
-    
-    @unwrap_json_response
-    @fail_if_not_ok("Failed to set wallet settings")
-    def put_subwallet_settings(self, wallet_id, content):
-        """Set connection metadata."""
-        return put(
-            self.url,
-            f"/multitenancy/wallet/{wallet_id}/",
             json={"content": content},
         )
 
