4c87d2774ed8632afcd6636c28a6ea8191f14f14
Akiff Manji <akiff.manji@quartech.com>
2024-01-12 16:06:52 -0800

    feat: scaffolding for rpc plugin
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

---
+++ rpc/integration/tests/test_example.py
@@ -0,0 +1,31 @@
+import pytest
+import time
+
+from . import Agent, BOB, ALICE
+
+
+@pytest.fixture(scope="session")
+def bob():
+    """bob agent fixture."""
+    yield Agent(BOB)
+
+
+@pytest.fixture(scope="session")
+def alice():
+    """resolver agent fixture."""
+    yield Agent(ALICE)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def established_connection(bob, alice):
+    """Established connection filter."""
+    invite = bob.create_invitation(auto_accept="true")["invitation"]
+    resp = alice.receive_invite(invite, auto_accept="true")
+    yield resp["connection_id"]
+
+
+def test_send_message(bob, alice, established_connection):
+    # make sure connection is active...
+    time.sleep(1)
+
+    alice.send_message(established_connection, "hello bob")
