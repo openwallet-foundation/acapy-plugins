88d28a603b7cfaa84c53c2ab1beb94f15f0cdf73
Akiff Manji <akiff.manji@quartech.com>
2024-01-19 19:36:06 +0000

    feat: required model fields and tests
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- rpc/rpc/v1_0/models.py
+++ rpc/rpc/v1_0/models.py
@@ -1,26 +1,68 @@
 """RPC Messages model classes and schemas."""
-from aries_cloudagent.messaging.models.base_record import BaseRecord, BaseRecordSchema
+from aries_cloudagent.messaging.models.base import BaseModel, BaseModelSchema
+from marshmallow import fields, validate
 
-class RPCRequestRecord(BaseRecord):
-  """RPC Request Record"""
+class RPCBaseModel(BaseModel):
+  """RPC Base Model"""
+
+  class Meta:
+    schema_class = 'RPCBaseModelSchema'
+
+  def __init__(self, jsonrpc):
+    super().__init__()
+    self.jsonrpc = jsonrpc
+
+class RPCRequestModel(RPCBaseModel):
+  """RPC Request Model"""
+
+  class Meta:
+    schema_class = 'RPCRequestModelSchema'
+
+  def __init__(self, jsonrpc, method):
+    super().__init__(jsonrpc)
+    self.method = method
+
+class RPCResponseModel(RPCBaseModel):
+  """RPC Response Model"""
   pass
 
-class RPCResponseRecord(BaseRecord):
-  """RPC Response Record"""
+class RPCErrorModel(BaseModel):
+  """RPC Error Model"""
+
+  class Meta:
+    schema_class = 'RPCErrorModelSchema'
+  
+  def __init__(self, code, message):
+    super().__init__()
+    self.code = code
+    self.message = message
+
+class RPCBaseModelSchema(BaseModelSchema):
+  """Schema to allow serialization/deserialization of RPC Base Models."""
+
+  class Meta:
+    model_class = 'RPCBaseModel'
+
+  jsonrpc = fields.String(required=True, validate=validate.Equal('2.0'))
+
+class RPCRequestModelSchema(RPCBaseModelSchema):
+  """Schema to allow serialization/deserialization of RPC Request Models."""
+
+  class Meta:
+    model_class = 'RPCRequestModel'
+
+  method = fields.String(required=True, validate=validate.Regexp(regex='^(?!rpc\.).*$',
+                                                                 error='Method name cannot be internal RPC method'))
+
+class RPCResponseModelSchema(RPCBaseModelSchema):
+  """Schema to allow serialization/deserialization of RPC Response Models."""
   pass
 
-class RPCErrorRecord(BaseRecord):
-  """RPC Error Record"""
-  pass
+class RPCErrorModelSchema(BaseModelSchema):
+  """Schema to allow serialization/deserialization of RPC Error Models."""
 
-class RPCRequestRecordSchema(BaseRecordSchema):
-  """Schema to allow serialization/deserialization of RPC Request Records."""
-  pass
+  class Meta:
+    model_class = 'RPCErrorModel'
 
-class RPCResponseRecordSchema(BaseRecordSchema):
-  """Schema to allow serialization/deserialization of RPC Response Records."""
-  pass
-
-class RPCErrorRecordSchema(BaseRecordSchema):
-  """Schema to allow serialization/deserialization of RPC Error Records."""
-  pass
+  code = fields.Integer(required=True)
+  message = fields.String(required=True)
---
+++ rpc/rpc/v1_0/tests/test_models.py
@@ -0,0 +1,106 @@
+import pytest
+
+from marshmallow import ValidationError
+from rpc.v1_0.models import RPCBaseModelSchema, RPCRequestModelSchema, RPCResponseModelSchema, RPCErrorModelSchema
+
+rpc_base = {
+  'jsonrpc': '2.0'
+}
+
+@pytest.mark.parametrize('test_input', [rpc_base])
+def test_valid_rpc_base(test_input):
+  schema = RPCBaseModelSchema()
+  result = schema.load(test_input)
+
+  assert result.jsonrpc == '2.0'
+
+@pytest.mark.parametrize('test_input', [{}])
+def test_invalid_rpc_base_jsonrpc_missing(test_input):
+  schema = RPCBaseModelSchema()
+  
+  with pytest.raises(ValidationError) as exc_info:
+    schema.load(test_input)
+
+  assert 'jsonrpc' in exc_info.value.messages
+  assert 'Missing data for required field.' in exc_info.value.messages['jsonrpc']
+
+@pytest.mark.parametrize('test_input', [{
+  **rpc_base,
+  'method': 'test.method'
+}])
+def test_valid_rpc_request(test_input):
+  schema = RPCRequestModelSchema()
+  result = schema.load(test_input)
+
+  assert result.jsonrpc == '2.0'
+  assert result.method == 'test.method'
+
+@pytest.mark.parametrize('test_input', [{
+  **rpc_base,
+  'method': 'rpc.test.method'
+}])
+def test_invalid_rpc_request_internal_method(test_input):
+  schema = RPCRequestModelSchema()
+  
+  with pytest.raises(ValidationError) as exc_info:
+    schema.load(test_input)
+  
+  assert 'method' in exc_info.value.messages
+  assert 'Method name cannot be internal RPC method' in exc_info.value.messages['method']
+
+@pytest.mark.parametrize('test_input', [{
+  'code': -123,
+  'message': 'Test error message'
+}, {
+  'code': 123,
+  'message': 'Test error message'
+}, {
+  'code': '-123',
+  'message': 'Test error message'
+}, {
+  'code': '123',
+  'message': 'Test error message'
+}])
+def test_valid_rpc_error(test_input):
+  schema = RPCErrorModelSchema()
+  result = schema.load(test_input)
+
+  assert result.code == int(test_input['code'])
+  assert result.message == test_input['message']
+
+@pytest.mark.parametrize('test_input', [{
+  'message': 'Test error message'
+}])
+def test_invalid_rpc_error_code_missing(test_input):
+  schema = RPCErrorModelSchema()
+
+  with pytest.raises(ValidationError) as exc_info:
+    schema.load(test_input)
+
+  assert 'code' in exc_info.value.messages
+  assert 'Missing data for required field.' in exc_info.value.messages['code']
+
+@pytest.mark.parametrize('test_input', [{
+  'code': 123
+}])
+def test_invalid_rpc_error_message_missing(test_input):
+  schema = RPCErrorModelSchema()
+
+  with pytest.raises(ValidationError) as exc_info:
+    schema.load(test_input)
+
+  assert 'message' in exc_info.value.messages
+  assert 'Missing data for required field.' in exc_info.value.messages['message']
+
+@pytest.mark.parametrize('test_input', [{
+  'code': 'abc',
+  'message': 'Test error message'
+}])
+def test_invalid_rpc_error_code_type(test_input):
+  schema = RPCErrorModelSchema()
+
+  with pytest.raises(ValidationError) as exc_info:
+    schema.load(test_input)
+
+  assert 'code' in exc_info.value.messages
+  assert 'Not a valid integer.' in exc_info.value.messages['code']
