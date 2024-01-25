29e6def689a5373c36641b569f4744a1f6d83d8a
Akiff Manji <akiff.manji@quartech.com>
2024-01-25 17:07:38 +0000

    feat: add plugin routes (WIP) and route tests
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- rpc/rpc/v1_0/routes.py
+++ rpc/rpc/v1_0/routes.py
@@ -1,46 +1,68 @@
-from typing import Union
+import logging
+from typing import Iterable
+
 from aiohttp import web
 from aiohttp_apispec import docs, request_schema, response_schema
-from marshmallow import fields
+from marshmallow import fields, post_load
 
+from aries_cloudagent.admin.request_context import AdminRequestContext
+from aries_cloudagent.connections.models.conn_record import ConnRecord
 from aries_cloudagent.messaging.models.openapi import OpenAPISchema
 from aries_cloudagent.messaging.valid import UUID4_EXAMPLE
-from aries_cloudagent.messaging.agent_message import AgentMessage, AgentMessageSchema
+from aries_cloudagent.messaging.models.base import BaseModel, BaseModelSchema
+from aries_cloudagent.storage.base import BaseStorage, StorageRecord
+from aries_cloudagent.storage.error import StorageNotFoundError, StorageError
 
 from rpc.v1_0.models import (
   RPC_REQUEST_EXAMPLE,
   RPC_RESPONSE_EXAMPLE,
-  DRPCRequestRecordSchema,
-  DRPCResponseRecordSchema,
+  DRPCRequestRecord,
   Request,
   Response
 )
+from rpc.v1_0.messages import (
+    DRPCRequestMessage,
+    DRPCRequestMessageSchema,
+    DRPCResponseMessageSchema
+)
+
+LOGGER = logging.getLogger(__name__)
 
 
-class DRPCRequestResult(AgentMessage):
-    """DIDComm RPC Request Result Agent Message."""
-
+class DRPCRequest(BaseModel):
+    """DIDComm RPC Request Model."""
+    
     class Meta:
-        schema_class = "DRPCRequestResultSchema"
+        schema_class = "DRPCRequestSchema"
 
-    def __init__(self, *, state: str = None, **kwargs):
+    def __init__(self, *, conn_id: str = None, request: dict = None, **kwargs):
         super().__init__(**kwargs)
-        self.state = state
+        self.conn_id = conn_id
+        self.request = request
 
 
-class DRPCResponseResult(AgentMessage):
-    """DIDComm RPC Response Result Agent Message."""
-
+class DRPCResponse(BaseModel):
+    """DIDComm RPC Response Model."""
+    
     class Meta:
-        schema_class = "DRPCResponseResultSchema"
+        schema_class = "DRPCResponseSchema"
 
-    def __init__(self, *, state: str = None, **kwargs):
+    def __init__(self, *, conn_id: str = None, response: dict = None, **kwargs):
         super().__init__(**kwargs)
-        self.state = state
+        self.conn_id = conn_id
+        self.response = response
 
 
-class DRPCRequestSchema(OpenAPISchema):
+class DRPCRequestSchema(BaseModelSchema, OpenAPISchema):
     """Request schema for sending a DIDComm RPC Request."""
+
+    class Meta:
+      model_class = "DRPCRequest"
+
+    conn_id = fields.String(
+        required=True,
+        metadata={"description": "Connection identifier", "example": UUID4_EXAMPLE},
+    )
 
     request = Request(
         required=True,
@@ -48,20 +70,16 @@ class DRPCRequestSchema(OpenAPISchema):
     )
 
 
-class DRPCRequestResultSchema(AgentMessageSchema, DRPCRequestRecordSchema):
-    """Result schema from sending a DIDComm RPC Request."""
+class DRPCResponseSchema(BaseModelSchema, OpenAPISchema):
+    """Request schema for sending a DIDComm RPC Response."""
 
     class Meta:
-      model_class = "DRPCRequestResult"
+      model_class = "DRPCResponse"
 
     conn_id = fields.String(
         required=True,
         metadata={"description": "Connection identifier", "example": UUID4_EXAMPLE},
     )
-
-
-class DRPCResponseSchema(OpenAPISchema):
-    """Request schema for sending a DIDComm RPC Response."""
 
     response = Response(
         required=True,
@@ -69,47 +87,96 @@ class DRPCResponseSchema(OpenAPISchema):
     )
 
   
-class DRPCResponseResultSchema(AgentMessageSchema, DRPCResponseRecordSchema):
-    """Result schema from sending a DIDComm RPC Response."""
-
-    class Meta:
-      model_class = "DRPCResponseResult"
-
-    conn_id = fields.String(
-        required=True,
-        metadata={"description": "Connection identifier", "example": UUID4_EXAMPLE},
-    )
-
-
 @docs(
     tags=["drpc"], summary="Send a DIDComm RPC request message",
 )
 @request_schema(DRPCRequestSchema())
-@response_schema(DRPCRequestResultSchema(), 200)
+@response_schema(DRPCRequestMessageSchema(), 200)
 async def drpc_send_request(request: web.BaseRequest):
     """
     Request handler for sending a DIDComm RPC request message.
     """
-    return web.json_response({})
+
+    LOGGER.debug("Recieved DRPC send request >>>")
+
+    context: AdminRequestContext = request["context"]
+    outbound_handler = request["outbound_message_router"]
+
+    body = await request.json()
+    conn_id = body["conn_id"]
+    request = body["request"]
+
+    async with context.session() as session:
+        storage = session.inject(BaseStorage)
+        try:
+            connection = await ConnRecord.retrieve_by_id(session, conn_id)
+        except StorageNotFoundError as err:
+            raise web.HTTPNotFound(reason=err.roll_up) from err
+
+        if (connection.is_ready):
+            request_record = DRPCRequestRecord(request=request)
+
+            # Save the request in the wallet
+            record = StorageRecord(
+                type=DRPCRequestRecord.RECORD_TYPE,
+                value=request_record.serialize()
+            )
+            try:
+                await storage.add_record(record)
+            except StorageError as err:
+                raise web.HTTPBadRequest(reason=err.roll_up) from err
+
+            # Create a new message to the recipient
+            msg = DRPCRequestMessage(conn_id=conn_id,
+                                    request=request_record.request,
+                                    state=request_record.state)
+            # TODO: Uncomment when testing against another agent with drpc enabled
+            await outbound_handler(msg, connection_id=conn_id)
+            return web.json_response(msg.serialize())
+    
+        raise web.HTTPForbidden(reason=f"Connection {conn_id} is not ready")
 
 
 @docs(
     tags=["drpc"], summary="Send a DIDComm RPC response message",
 )
 @request_schema(DRPCResponseSchema())
-@response_schema(DRPCResponseResultSchema(), 200)
+@response_schema(DRPCResponseMessageSchema(), 200)
 async def drpc_send_response(request: web.BaseRequest):
     """
     Request handler for sending a DIDComm RPC response message.
     """
-    return web.json_response({})
+
+    LOGGER.debug("Recieved DRPC send response >>>")
+
+    context: AdminRequestContext = request["context"]
+    outbound_handler = request["outbound_message_router"]
+
+    body = await request.json()
+    conn_id = body["conn_id"]
+    response = body["response"]
+
+    async with context.session() as session:
+        storage = session.inject(BaseStorage)
+        try:
+            connection = await ConnRecord.retrieve_by_id(session, conn_id)
+        except StorageNotFoundError as err:
+            raise web.HTTPNotFound(reason=err.roll_up) from err
+
+        if (connection.is_ready):
+            # TODO:
+            pass
+
+            return web.json_response({})
+    
+        raise web.HTTPForbidden(reason=f"Connection {conn_id} is not ready")
 
 
 async def register(app: web.Application):
     """Register routes."""
 
-    app.add_routes([web.post("/drpc/{conn_id}/request", drpc_send_request)])
-    app.add_routes([web.post("/drpc/{conn_id}/response", drpc_send_response)])
+    app.add_routes([web.post("/drpc/request", drpc_send_request)])
+    app.add_routes([web.post("/drpc/response", drpc_send_response)])
 
 
 def post_process_routes(app: web.Application):
@@ -119,5 +186,5 @@ def post_process_routes(app: web.Application):
     if "tags" not in app._state["swagger_dict"]:
         app._state["swagger_dict"]["tags"] = []
     app._state["swagger_dict"]["tags"].append(
-        {"name": "drpc", "description": "DIDComm RPC between connections"}
+        {"name": "drpc", "description": "DIDComm RPC between Aries agents"}
     )
---
+++ rpc/rpc/v1_0/tests/test_routes.py
@@ -0,0 +1,133 @@
+from marshmallow import ValidationError
+import pytest
+from rpc.v1_0.routes import DRPCRequestResultSchema, DRPCRequestSchema, DRPCResponseResultSchema, DRPCResponseSchema
+
+
+def test_valid_drpc_request():
+    """Test the DRPCRequestSchema schema."""
+
+    data = {
+        "conn_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "request": {
+            "jsonrpc": "2.0",
+            "method": "test.method",
+            "id": "1",
+            "params": {"one": "1"},
+        },
+    }
+
+    schema = DRPCRequestSchema()
+    result = schema.load(data)
+    assert result.conn_id == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    assert result.request.jsonrpc == "2.0"
+    assert result.request.method == "test.method"
+    assert result.request.id == "1"
+    assert result.request.params == {"one": "1"}
+
+
+def test_invalid_drpc_request():
+    """Test the DRPCRequestSchema schema."""
+
+    data = {
+        "conn_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "request": {
+            "method": "test.method",
+            "id": "1",
+            "params": {"one": "1"},
+        },
+    }
+
+    schema = DRPCRequestSchema()
+
+    with pytest.raises(ValidationError) as exc_info:
+      schema.load(data)
+
+    assert 'request' in exc_info.value.messages
+    assert 'jsonrpc' in exc_info.value.messages['request']
+    assert 'Missing data for required field.' in exc_info.value.messages['request']['jsonrpc']
+
+
+def test_valid_drpc_request_result():
+    """Test the DRPCRequesResultSchema schema."""
+
+    data = {
+        "conn_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "request": {
+            "jsonrpc": "2.0",
+            "method": "test.method",
+            "id": "1",
+            "params": {"one": "1"},
+        },
+    }
+
+    schema = DRPCRequestResultSchema()
+    result = schema.load(data)
+    assert result._id is not None
+    assert result._type is not None
+    assert result.conn_id == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    assert result.request.jsonrpc == "2.0"
+    assert result.request.method == "test.method"
+    assert result.request.id == "1"
+    assert result.request.params == {"one": "1"}
+
+def test_valid_drpc_response():
+    """Test the DRPCResponseSchema schema."""
+
+    data = {
+        "conn_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "response": {
+            "jsonrpc": "2.0",
+            "result": "test result",
+            "id": "1",
+        },
+    }
+
+    schema = DRPCResponseSchema()
+    result = schema.load(data)
+    assert result.conn_id == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    assert result.response.jsonrpc == "2.0"
+    assert result.response.result == "test result"
+    assert result.response.id == "1"
+
+
+def test_invalid_drpc_response():
+    """Test the DRPCResponseSchema schema."""
+
+    data = {
+        "conn_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "response": {
+            "result": "test result",
+            "id": "1",
+        },
+    }
+
+    schema = DRPCResponseSchema()
+
+    with pytest.raises(ValidationError) as exc_info:
+      schema.load(data)
+
+    assert 'response' in exc_info.value.messages
+    assert 'jsonrpc' in exc_info.value.messages['response']
+    assert 'Missing data for required field.' in exc_info.value.messages['response']['jsonrpc']
+
+
+def test_valid_drpc_response_result():
+    """Test the DRPCResponseResultSchema schema."""
+
+    data = {
+        "conn_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "response": {
+            "jsonrpc": "2.0",
+            "result": "test result",
+            "id": "1",
+        },
+    }
+
+    schema = DRPCResponseResultSchema()
+    result = schema.load(data)
+    assert result._id is not None
+    assert result._type is not None
+    assert result.conn_id == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    assert result.response.jsonrpc == "2.0"
+    assert result.response.result == "test result"
+    assert result.response.id == "1"
