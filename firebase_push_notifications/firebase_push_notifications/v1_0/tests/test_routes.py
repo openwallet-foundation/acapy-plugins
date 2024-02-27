103fd4c5c64934e97bb815bff6194f841e661480
jamshale <jamiehalebc@gmail.com>
2024-02-27 19:00:26 +0000

    Add firebase push notification plugin
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

---
+++ firebase_push_notifications/firebase_push_notifications/v1_0/tests/test_routes.py
@@ -0,0 +1,47 @@
+from aiohttp import web
+from aries_cloudagent.admin.request_context import AdminRequestContext
+from aries_cloudagent.connections.models.conn_record import ConnRecord
+from aries_cloudagent.core.event_bus import MockEventBus
+from aries_cloudagent.storage.error import StorageNotFoundError
+from asynctest import TestCase as AsyncTestCase
+from asynctest import mock
+
+from .. import routes as test_module
+
+
+class TestRoutes(AsyncTestCase):
+    async def setUp(self) -> None:
+        self.session_inject = {}
+        self.context = AdminRequestContext.test_context(self.session_inject)
+        self.request_dict = {"context": self.context}
+        self.request = mock.MagicMock(
+            app={},
+            match_info={},
+            query={},
+            __getitem__=lambda _, k: self.request_dict[k],
+        )
+
+    async def test_register_events_subscribes_to_event_bus(self):
+        mock_event_bus = MockEventBus()
+        mock_event_bus.subscribe = mock.MagicMock()
+        test_module.register_events(mock_event_bus)
+        self.assertEqual(mock_event_bus.subscribe.called, True)
+
+    @mock.patch.object(
+        ConnRecord, "retrieve_by_id", side_effect=[StorageNotFoundError("test")]
+    )
+    async def test_set_connection_device_info_no_connection(self, mock_conn):
+        self.request.match_info = {"conn_id": "test-conn-id"}
+        with self.assertRaises(web.HTTPNotFound):
+            await test_module.set_connection_device_info(self.request)
+            assert mock_conn.called
+
+    @mock.patch.object(ConnRecord, "retrieve_by_id", return_value=None)
+    @mock.patch.object(test_module, "save_device_token")
+    async def test_set_connection_device_info_with_connection(
+        self, mock_save_info, mock_conn
+    ):
+        self.request.match_info = {"conn_id": "test-conn-id"}
+        await test_module.set_connection_device_info(self.request)
+        assert mock_conn.called
+        assert mock_save_info.called
