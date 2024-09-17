cdb9033308fe42e8e16175b553b7cf8601459898
Micah Peltier <micah6_8@yahoo.com>
2024-09-17 12:35:17 -0600

    feat: add setup for sd jwts
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

---
+++ oid4vc/sd_jwt_vc/__init__.py
@@ -0,0 +1,18 @@
+"""SD-JWT Crendential Handler Plugin."""
+
+from importlib.util import find_spec
+
+from aries_cloudagent.config.injection_context import InjectionContext
+from sd_jwt_vc.cred_processor import SdJwtCredIssueProcessor
+from oid4vc.cred_processor import CredProcessors
+
+jsonpointer = find_spec("jsonpointer")
+if not jsonpointer:
+    raise ImportError("`sd_jwt` extra required")
+
+
+async def setup(context: InjectionContext):
+    """Setup the plugin."""
+    processors = context.inject(CredProcessors)
+    sd_jwt = SdJwtCredIssueProcessor()
+    processors.register(sd_jwt)
