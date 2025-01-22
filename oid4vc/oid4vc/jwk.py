7bc8c475e3316cb0182bfc3e0c3ba693f37f2206
jamshale <jamiehalebc@gmail.com>
2025-01-22 17:29:49 +0000

    Fix KeyType required param
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- oid4vc/oid4vc/jwk.py
+++ oid4vc/oid4vc/jwk.py
@@ -3,6 +3,6 @@
 from acapy_agent.wallet.did_method import DIDMethod
 from acapy_agent.wallet.key_type import ED25519, KeyType
 
-P256: KeyType = KeyType("p256", "p256-pub", b"\x12\x00")
+P256: KeyType = KeyType("p256", "p256-pub", b"\x12\x00", jws_alg=None)
 
 DID_JWK = DIDMethod("jwk", [ED25519, P256])
