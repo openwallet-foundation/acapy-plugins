eef488b00e1132ce647ac6198f03b7d30cbbd4b1
jamshale <jamiehalebc@gmail.com>
2025-01-28 22:20:27 +0000

    Load connections didcomm protocols
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- connections/connections/__init__.py
+++ connections/connections/__init__.py
@@ -1,13 +1,15 @@
 """Integrate Connections Protocol Plugin."""
 
 import logging
+
 from acapy_agent.config.injection_context import InjectionContext
 from acapy_agent.config.provider import ClassProvider
 from acapy_agent.connections.base_manager import BaseConnectionManager
 from acapy_agent.core.profile import Profile
+from acapy_agent.core.protocol_registry import ProtocolRegistry
 
 from connections.v1_0.manager import ConnectionManager
-
+from connections.v1_0.message_types import MESSAGE_TYPES
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,3 +21,7 @@ async def setup(context: InjectionContext):
         BaseConnectionManager,
         ClassProvider(ConnectionManager, ClassProvider.Inject(Profile)),
     )
+    # Did-comm message types
+    protocol_registry = context.inject(ProtocolRegistry)
+    LOGGER.debug("Registering connection v1 message types")
+    protocol_registry.register_message_types(MESSAGE_TYPES)
