4d60594c19ea94528be8d50e157128b3e7d911cd
jamshale <jamiehalebc@gmail.com>
2025-04-24 21:12:19 +0000

    Fix anoncreds unit tests
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- webvh/webvh/anoncreds/tests/test_registry.py
+++ webvh/webvh/anoncreds/tests/test_registry.py
@@ -138,7 +138,9 @@ class TestAnoncredsRegistry(IsolatedAsyncioTestCase):
                 name="test_schema",
                 version="1.0",
             ),
-            {},
+            {
+                "verificationMethod": f"{test_issuer_id}#key-01",
+            },
         )
         assert isinstance(result, SchemaResult)
         assert result.schema_state.state == "finished"
@@ -198,7 +200,9 @@ class TestAnoncredsRegistry(IsolatedAsyncioTestCase):
                     primary=CredDefValuePrimary("n", "s", {}, "rctxt", "z")
                 ),
             ),
-            {},
+            {
+                "verificationMethod": f"{test_issuer_id}#key-01",
+            },
         )
         assert isinstance(result, CredDefResult)
         assert result.credential_definition_state.state == "finished"
@@ -262,7 +266,9 @@ class TestAnoncredsRegistry(IsolatedAsyncioTestCase):
                 issuer_id=test_issuer_id,
                 type="CL_ACCUM",
             ),
-            {},
+            {
+                "verificationMethod": f"{test_issuer_id}#key-01",
+            },
         )
         assert isinstance(result, RevRegDefResult)
         assert result.revocation_registry_definition_state.state == "finished"
@@ -351,6 +357,9 @@ class TestAnoncredsRegistry(IsolatedAsyncioTestCase):
                 timestamp=1669640864487,
                 rev_reg_def_id=test_rev_reg_id,
             ),
+            {
+                "verificationMethod": f"{test_issuer_id}#key-01",
+            },
         )
         assert isinstance(result, RevListResult)
         assert result.revocation_list_state.state == "finished"
