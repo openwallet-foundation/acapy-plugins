3f343d696be51d57d35300da22b6ca29062303a2
jamshale <jamiehalebc@gmail.com>
2025-11-13 21:46:04 +0000

    feat: use network cleaup script and random subnet to avoid conflicts
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- cache_redis/integration/tests/conftest.py
+++ cache_redis/integration/tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest_asyncio
 from redis.asyncio import RedisCluster
 from redis import asyncio as aioredis
@@ -6,7 +7,8 @@ from redis import asyncio as aioredis
 @pytest_asyncio.fixture
 async def redis_cluster_client():
     """Yield RedisCluster client."""
-    redis = RedisCluster.from_url(url="redis://default:test1234@172.28.0.103:6382")
+    subnet_prefix = os.environ.get("SUBNET_PREFIX", "172.28")
+    redis = RedisCluster.from_url(url=f"redis://default:test1234@{subnet_prefix}.0.103:6382")
     yield redis
     await redis.close()
 
