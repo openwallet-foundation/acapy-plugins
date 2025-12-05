0a2ad4b54562e2a00bc0104c41a44f5f88ffc4af
jamshale <jamiehalebc@gmail.com>
2025-12-05 10:24:53 -0800

    chore: clean up some default and common values
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- issue_credential/.devcontainer/post-install.sh
+++ issue_credential/.devcontainer/post-install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -ex
-
-# Convenience workspace directory for later use
-WORKSPACE_DIR=$(pwd)
-
-# install all ACA-Py requirements
+ 
 python -m pip install --upgrade pip
 
 # Generate Poetry Lock file
--- rpc/.devcontainer/post-install.sh
+++ rpc/.devcontainer/post-install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -ex
-
-# Convenience workspace directory for later use
-WORKSPACE_DIR=$(pwd)
-
-# install all ACA-Py requirements
+ 
 python -m pip install --upgrade pip
